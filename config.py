8d71a7f298c8a4e7090463e0590bbba9cd09aadb
maikl161973 <bmihaill16@yandex.ru>
2025-03-28 11:50:43 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+
+CryptoCurrencyParams = {
+    'host': 'https://min-api.cryptocompare.com',
+    'api_url': '/data/price',
+    'fsim_param': 'fsym',
+    'tsim_param': 'tsyms',
+    'timeout': 300
+}
+
+TOKEN = '8002285611:AAHjP9zc3Th_2i3joekeBSBzwW5Y5dXBeEQ'
+
+BotParams = {
+    'currency': {
+        'евро': 'EUR',
+        'доллар': 'USD',
+        'юань': 'CNY',
+        'иена': 'JPY'
+    }
+}
